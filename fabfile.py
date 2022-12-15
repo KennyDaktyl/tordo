87bb22cbc3099cdba3f220c1563fa0b1346cea77
KennyDaktyl <michal.pielak81@gmail.com>
2022-12-15 14:35:38 +0100

    deploy z fabric

--- fabfile.py
+++ fabfile.py
@@ -7,10 +7,10 @@ from contextlib import contextmanager as _contextmanager
 
 local_user = "michalp"
 
-env.hosts = ['51.75.64.242']
-env.port = '62211'
-env.user = 'kenny'
-env.password = 'Tofik123!'
+env.hosts = os.environ.get("IP_HOST")
+env.port = os.environ.get("SSH_PORT")
+env.user = os.environ.get("HOST_USER")
+env.password = os.environ.get("SSH_PASSWORD")
 env.key_filename = '/home/michalp/.ssh/id_rsa_vps'
 env.directory = '/home/kenny/www/tordo'
 env.activate = 'source {}/env/bin/activate'.format(env.directory)
@@ -44,9 +44,7 @@ def backup_db():
     run('sudo PGPASSWORD="' + password +
           '" pg_dump -U postgres -h localhost tordo_dev > /home/kenny/www/tordo/media/files/tordo_dev_backup.sql')
     local(f'sshpass -p {os.environ.get("SSH_PASSWORD")} scp -r -P 62211 kenny@51.75.64.242:/home/kenny/www/tordo/media/files/tordo_dev_backup.sql /home/{local_user}/Pulpit/Backup')
-    local(f'PGPASSWORD="{os.environ.get("SSH_PASSWORD")} dropdb -U postgres -h localhost tordo_dev')
-    local(f'PGPASSWORD="{os.environ.get("SSH_PASSWORD")} createdb -U postgres -h localhost tordo_dev')
-    local(f'PGPASSWORD="Tofik123!" psql -U postgres -h localhost tordo_dev < /home/{local_user}/Pulpit/tordo_dev_backup.sql')
+
 
 
 #  fab -f fabfile.py restore_local_db
@@ -62,5 +60,5 @@ def restore_local_db():
 def push(message=""):
     local('git status')
     local('git add .')
-    local('git commit -m "' + message + '"')
+    local('git commit -m "deploy z fabric"')
     local('git push')
