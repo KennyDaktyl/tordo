6fd0e71b1a6f36b070f20bf4a84fd227a7ed3229
KennyDaktyl <michal.pielak81@gmail.com>
2022-12-15 14:24:06 +0100

    deploy z fabric

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,62 @@
 from __future__ import with_statement
+
+import os
+
 from fabric.api import *
+from contextlib import contextmanager as _contextmanager
+
+local_user = "michalp"
 
 env.hosts = ['51.75.64.242']
 env.port = '62211'
 env.user = 'kenny'
+env.password = 'Tofik123!'
 env.key_filename = '/home/michalp/.ssh/id_rsa_vps'
+env.directory = '/home/kenny/www/tordo'
+env.activate = 'source {}/env/bin/activate'.format(env.directory)
+env.export = f'export DB_USER={os.environ.get("DB_USER")} && export DB_PASSWORD={os.environ.get("DB_PASSWORD")}'.format(env.directory)
 
 
+@_contextmanager
+def virtualenv():
+    with cd(env.directory):
+        with prefix(env.activate):
+            with prefix(env.export):
+                yield
+
+
+#  fab -f fabfile.py deploy
 def deploy():
-    with cd('/home/kenny/www/tordo'):
-        # run('git pull')
-        run('sudo source env/bin/activate')
-        run('sudo source env/export.txt')
-        run('python manage.py collectstatic --noinput')
-        run('python manage.py compress --force')
+    local('git add .')
+    local('git commit -m "deploy z fabric"')
+    local('git push')
+    with virtualenv():
+        run('sudo git pull')
+        run('sudo /home/kenny/www/tordo/env/bin/python manage.py collectstatic --noinput')
+        run('sudo /home/kenny/www/tordo/env/bin/python manage.py compress --force')
+        run('python manage.py migrate')
         run('sudo systemctl restart tordo.service')
+
+
+#  fab -f fabfile.py backup_db
+def backup_db():
+    password = os.environ.get('DB_PASSWORD')
+    run('sudo PGPASSWORD="' + password +
+          '" pg_dump -U postgres -h localhost tordo_dev > /home/kenny/www/tordo/media/files/tordo_dev_backup.sql')
+    local(f'sshpass -p {os.environ.get("SSH_PASSWORD")} scp -r -P 62211 kenny@51.75.64.242:/home/kenny/www/tordo/media/files/tordo_dev_backup.sql /home/{local_user}/Pulpit/Backup')
+    local(f'PGPASSWORD="{os.environ.get("SSH_PASSWORD")} dropdb -U postgres -h localhost tordo_dev')
+    local(f'PGPASSWORD="{os.environ.get("SSH_PASSWORD")} createdb -U postgres -h localhost tordo_dev')
+    local(f'PGPASSWORD="Tofik123!" psql -U postgres -h localhost tordo_dev < /home/{local_user}/Pulpit/tordo_dev_backup.sql')
+
+
+#  fab -f fabfile.py restore_local_db
+def restore_local_db():
+    local(
+        f'sshpass -p {os.environ.get("SSH_PASSWORD")} scp -r -P 62211 kenny@51.75.64.242:/home/kenny/www/tordo/media/files/tordo_dev_backup.sql /home/{local_user}/Pulpit/Backup')
+    local(f'PGPASSWORD={os.environ.get("SSH_PASSWORD")} dropdb -U postgres -h localhost tordo_dev')
+    local(f'PGPASSWORD={os.environ.get("SSH_PASSWORD")} createdb -U postgres -h localhost tordo_dev')
+    local(
+        f'PGPASSWORD={os.environ.get("SSH_PASSWORD")} psql -U postgres -h localhost tordo_dev < /home/{local_user}/Pulpit/Backup/tordo_dev_backup.sql')
 
 
 def push(message=""):
