7e8c8f459e0dac8315a1cb92d4aa02878c54decd
Michał Pielak <toja@example.com>
2022-12-25 12:07:16 +0100

    added mobile

--- web/restaurants/views_api.py
+++ web/restaurants/views_api.py
@@ -14,9 +14,10 @@ FilterMapping = {
 
 
 class CountRestaurantWhenUseFilters(APIView):
-    def get(self, request, format=None):
+    def get(self, request):
         restaurants = Restaurant.objects.filter(is_active=True)
-        checked = json.loads(request.GET.get("checked", "false"))
+        checked = bool(request.GET.get("checked", False))
+        reset = request.GET.get("reset", False)
         filter_id = request.GET.get("filter_id")
 
         if filter_id:
@@ -25,23 +26,31 @@ class CountRestaurantWhenUseFilters(APIView):
             filter_session = request.session.get(filter_name, [])
             if request.session.get(filter_name):
                 if checked and int(id) not in filter_session:
-                    request.session[filter_name].append(int(id))
+                    filter_session.append(int(id))
                 else:
-                    request.session[filter_name].remove(int(id))
+                    filter_session.remove(int(id))
             else:
                 filter_object_id = filter_object.objects.get(pk=int(id)).id
-                request.session[filter_name] = [filter_object_id]
-                filter_session = request.session[filter_name]
+                filter_session = [filter_object_id]
 
             request.session[filter_name] = filter_session
 
         keys = list(FilterMapping.keys())
         if request.session.get(keys[0]):
-            restaurants = restaurants.filter(filter_foods__id__in=request.session.get(keys[0])).distinct()
+            if reset:
+                del request.session[keys[0]]
+            else:
+                restaurants = restaurants.filter(filter_foods__id__in=request.session.get(keys[0])).distinct()
         if request.session.get(keys[1]):
-            restaurants = restaurants.filter(filter_advantages__id__in=request.session.get(keys[1])).distinct()
+            if reset:
+                del request.session[keys[1]]
+            else:
+                restaurants = restaurants.filter(filter_advantages__id__in=request.session.get(keys[1])).distinct()
         if request.session.get(keys[2]):
-            restaurants = restaurants.filter(tags__id__in=request.session.get(keys[2])).distinct()
+            if reset:
+                del request.session[keys[2]]
+            else:
+                restaurants = restaurants.filter(tags__id__in=request.session.get(keys[2])).distinct()
 
         count_restaurants = {"count": restaurants.count()}
         return Response(CountRestaurantWhenUseFilterSerializer(count_restaurants).data)
